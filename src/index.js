fa4b9ea35fdabd176f9378837798adbde86369f6
uttam kumar <uttamdesigner@gmail.com>
2026-08-12 09:29:40 +0530

    change seeeing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import clientRoute from './routes/clientRoute.js'
 
 const app = express();
-const port = 3200;
+const port = 'https://crud-backend-orcin.vercel.app/';
 app.use(cors())
 app.use(express.json())
 app.use('/api', clientRoute)
